18578b9cec9decddc276fff956cee0721410e20c
perryzou <perryzou@foxmail.com>
2021-01-09 10:41:25 +0800

    定时推送天气

--- weather.py
+++ weather.py
@@ -1,51 +1,83 @@
 import urllib.parse
 
 import requests
+import sys, getopt
 
 
-def send(msg):
-    print(msg)
-    url = "https://api.day.app/MQiDfGtNgX3jAgCRKmohrf/今日天气预报/" + urllib.parse.quote(msg)
-    requests.get(url)
-    pass
+class WeatherTask:
+    def __init__(self, b, w, c):
+        self.b = b
+        self.w = w
+        self.c = c
+
+    def send(self, msg):
+        print(msg)
+        url = "https://api.day.app/%s/今日天气预报/%s" % (self.b, urllib.parse.quote(msg))
+        # url = "https://api.day.app/MQiDfGtNgX3jAgCRKmohrf/今日天气预报/" + urllib.parse.quote(msg)
+        requests.get(url)
+        pass
+
+    # 获取今天当前部分信息数据
+    def now_weather(self, ):
+        # url = 'https://devapi.heweather.net/v7/weather/now?location=101270119&key=818c832c460b46cd84e4ad34b75aca26'
+        url = 'https://devapi.heweather.net/v7/weather/now?location=%s&key=%s' % (self.c, self.w)
+        now_res = requests.get(url)
+        now_res_json = now_res.json()
+
+        # 获取信息返回
+        return now_res_json['now']
+
+    def now_and_future_twodays_weather(self):
+        url = 'https://devapi.heweather.net/v7/weather/3d?location=%s&key=%s' % (self.c, self.w)
+        forecast_res = requests.get(url)
+        forecast_res_json = forecast_res.json()
+        updateTime = forecast_res_json['updateTime']
+        daily_forecast = forecast_res_json['daily']
+        return updateTime, daily_forecast
+
+    def getRemark(self):
+        url = "https://v1.hitokoto.cn/"
+        forecast_res = requests.get(url)
+        return forecast_res.json()
+
+    def main(self):
+        now = self.now_weather()
+        updateTime, daily_forecast = self.now_and_future_twodays_weather()
+        remark = self.getRemark()
+
+        today = daily_forecast[0]
+        msg = '''当前天气：%s\t|\t气温：%s度\t|\t体感：%s度\t\n夜间天气：%s\t|\t最高：%s度\t|\t最低：%s度\t\n%s\t--%s''' \
+              % (now['text'], now['temp'], now['feelsLike'], today['textNight'], today['tempMax'], today['tempMin'],
+                 remark['hitokoto'], remark['from'])
+        self.send(msg)
 
 
-# 获取今天当前部分信息数据
-def now_weather():
-    url = 'https://devapi.heweather.net/v7/weather/now?location=101270119&key=818c832c460b46cd84e4ad34b75aca26'
-    now_res = requests.get(url)
-    now_res_json = now_res.json()
-
-    # 获取信息返回
-    return now_res_json['now']
-
-
-def now_and_future_twodays_weather():
-    url = 'https://devapi.heweather.net/v7/weather/3d?location=101270119&key=818c832c460b46cd84e4ad34b75aca26'
-    forecast_res = requests.get(url)
-    forecast_res_json = forecast_res.json()
-    updateTime = forecast_res_json['updateTime']
-    daily_forecast = forecast_res_json['daily']
-    return updateTime, daily_forecast
-
-def getRemark():
-    url="https://v1.hitokoto.cn/"
-    forecast_res = requests.get(url)
-    return forecast_res.json()
-
-
-
-def main():
-    now = now_weather()
-    updateTime, daily_forecast = now_and_future_twodays_weather()
-    remark = getRemark()
-
-    today = daily_forecast[0]
-    msg = '''当前天气：%s\t|\t气温：%s度\t|\t体感：%s度\t\n夜间天气：%s\t|\t最高：%s度\t|\t最低：%s度\t\n%s\t--%s''' \
-          % (now['text'], now['temp'], now['feelsLike'], today['textNight'], today['tempMax'], today['tempMin'],
-             remark['hitokoto'],remark['from'])
-    send(msg)
+def getArg(argv):
+    try:
+        options, args = getopt.getopt(argv, "b:w:c:")
+    except getopt.GetoptError:
+        sys.exit()
+    b = ""
+    w = ""
+    c = ""
+    for option, value in options:
+        if option == '-b':
+            b = value
+        if option == '-w':
+            w = value
+        if option == '-c':
+            c = value
+    return b, w, c
 
 
 if __name__ == "__main__":
-    main()
+    b, w, c = getArg(sys.argv[1:])
+    if b == '':
+        print("请设置push key")
+    elif w == '':
+        print("请设置天气key")
+    elif c == '':
+        print("请设置城市代码")
+    else:
+        task = WeatherTask(b, w, c)
+        task.main()
