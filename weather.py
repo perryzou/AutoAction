da7478eaf79fabb779454a4a746944eace1574ec
邹政 <zouzheng.zouzheng@alibaba-inc.com>
2023-08-17 14:49:37 +0800

    update weather location

--- weather.py
+++ weather.py
@@ -1,59 +1,78 @@
+import getopt
+import sys
+from datetime import datetime
 import urllib.parse
-
 import requests
-import sys, getopt
+import json
 
 
-class WeatherTask:
-    def __init__(self, b, w, c):
-        self.b = b
-        self.w = w
-        self.c = c
+def get_current_weather(location, key):
+    url = f"https://devapi.qweather.com/v7/weather/now?location={location}&key={key}"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    current_weather = data['now']
 
-    def send(self, msg):
-        print(msg)
-        url = "https://api.day.app/%s/今日天气预报/%s" % (self.b, urllib.parse.quote(msg))
-        requests.get(url)
-        pass
+    return data['fxLink'], \
+           f"{current_weather['text']} 温度:{current_weather['temp']}℃ 体感:{current_weather['feelsLike']}℃ "
 
-    # 获取今天当前部分信息数据
-    def now_weather(self, ):
-        url = 'https://devapi.heweather.net/v7/weather/now?location=%s&key=%s' % (self.c, self.w)
-        now_res = requests.get(url)
-        now_res_json = now_res.json()
 
-        # 获取信息返回
-        return now_res_json['now']
+def get_current_air(location, key):
+    url = f"https://devapi.qweather.com/v7/air/now?location={location}&key={key}"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    current_air = data['now']
+    return f"空气:{current_air['category']} {current_air['aqi']}\n"
 
-    def now_and_future_twodays_weather(self):
-        url = 'https://devapi.heweather.net/v7/weather/3d?location=%s&key=%s' % (self.c, self.w)
-        forecast_res = requests.get(url)
-        forecast_res_json = forecast_res.json()
-        updateTime = forecast_res_json['updateTime']
-        daily_forecast = forecast_res_json['daily']
-        return updateTime, daily_forecast
 
-    def getRemark(self):
-        url = "https://v1.hitokoto.cn/"
-        forecast_res = requests.get(url)
-        return forecast_res.json()
+def get_current_warning(location, key):
+    url = f"https://devapi.qweather.com/v7/warning/now?location={location}&key={key}"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    warning = data['warning']
+    msg = ""
+    for w in warning:
+        msg = msg + f"{w['title']}\n"
+    return msg
 
-    def main(self):
-        now = self.now_weather()
-        updateTime, daily_forecast = self.now_and_future_twodays_weather()
-        remark = self.getRemark()
 
-        today = daily_forecast[0]
-        msg = '''当前天气：%s\t|\t气温：%s度\t|\t体感：%s度\t\n夜间天气：%s\t|\t最高：%s度\t|\t最低：%s度\t\n%s\t--%s''' \
-              % (now['text'], now['temp'], now['feelsLike'], today['textNight'], today['tempMax'], today['tempMin'],
-                 remark['hitokoto'], remark['from'])
-        self.send(msg)
+def get_hourly_weather(location, key):
+    url = f"https://devapi.qweather.com/v7/weather/24h?location={location}&key={key}"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    hourly_weather = data['hourly']
+    msg = ""
+    for hour in hourly_weather[:2]:
+        fx_time = hour['fxTime']
+        dt = datetime.fromisoformat(fx_time)
+        formatted_time = dt.strftime("%H:%M")
+        msg = msg + f"{formatted_time}:{hour['text']} 温度:{hour['temp']}℃ 降雨:{hour['pop']}% {hour['precip']}mm\n"
+    return msg
+
+
+def get_daily_weather(location, key):
+    url = f"https://devapi.qweather.com/v7/weather/3d?location={location}&key={key}"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    daily_weather = data['daily']
+    tomorrow = daily_weather[1]
+    max_temp = tomorrow['tempMax']
+    min_temp = tomorrow['tempMin']
+    weather_text_day = tomorrow['textDay']
+    weather_text_night = tomorrow['textNight']
+    return f"明天:{min_temp}℃-{max_temp}℃ 白天:{weather_text_day} 夜晚:{weather_text_night}"
+
+
+def send(key, title, msg, link):
+    print(msg)
+    url = "https://api.day.app/%s/%s/%s?url=%s" % (key, title, urllib.parse.quote(msg), link)
+    requests.get(url)
 
 
 def getArg(argv):
     try:
         options, args = getopt.getopt(argv, "b:w:c:")
     except getopt.GetoptError:
+        print('arg error')
         sys.exit()
     b = ""
     w = ""
@@ -77,5 +96,14 @@ if __name__ == "__main__":
     elif c == '':
         print("请设置城市代码")
     else:
-        task = WeatherTask(b, w, c)
-        task.main()
+        locations = c.split("|")
+        for location in locations:
+            split = location.split(":")
+            location_name = split[0]
+            link, msg = get_current_weather(split[1], w)
+            msg = msg + get_current_air(split[1], w)
+            msg = msg + get_current_warning(split[1], w)
+            msg = msg + get_hourly_weather(split[1], w)
+            msg = msg + get_daily_weather(split[1], w)
+
+            send(b, location_name + '天气预报', msg, link)
